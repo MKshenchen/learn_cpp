8c49a24e560f453b2f3547e7cb3cfb583d23720b
MKshenchen <215096425@qq.com>
2024-03-06 15:08:54 +0800

    add create_lib

---
+++ create_lib/test/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "sort.h"
+
+int main() {
+    int i;
+    int arr[7] = {9, 6, 1, 8, 4, 0, 2};
+    
+    std::cout << "ori's arr: ";
+    for(i = 0; i < 7; std::cout << arr[i++] << " ");
+    std::cout << std::endl;
+
+    quickSort(arr, 0, 6);
+
+    std::cout << "after sort: ";
+    for(i = 0; i < 7; std::cout << arr[i++] << " ");
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ create_lib/test/sort.h
@@ -0,0 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
+void quickSort(int *arr, int low, int high);
+
+#endif
